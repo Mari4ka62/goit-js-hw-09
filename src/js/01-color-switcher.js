c2e0c44da20daa3b93e559ef5c9c91e03c07a068
Maria Vovchuk <mari4ka62@gmail.com>
2023-03-17 16:50:17 +0200

    Task1 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const startButton = document.querySelector('button[data-start]');
+const stopButton = document.querySelector('button[data-stop]');
 
+startButton.addEventListener('click', onStartButtonClick);
+stopButton.addEventListener('click', onStopButtonClick);
+let startInterval;
+function onStartButtonClick(e) {
+    startInterval = setInterval(getBodyColor, 1000);
+
+    startButton.disabled = true;
+    stopButton.disabled = false;
+    
+}
+function getBodyColor() {
+    document.body.style.backgroundColor=getRandomHexColor();
+}
+ 
+function onStopButtonClick(e) {
+    clearInterval(startInterval);
+    startButton.disabled = false;
+    stopButton.disabled = true;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}

6ce97aef80fe7839d10d3121ae730cbc70c6144b
Maria Vovchuk <mari4ka62@gmail.com>
2023-03-19 19:00:45 +0200

    Task 3 done

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,38 @@
+import { Notify } from "notiflix";
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', onFormSubmit);
+ 
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
+  
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+     resolve({position,delay})
   } else {
-    // Reject
+    reject({position,delay})
   }
+    }, delay)
+  })
+}
+
+function onFormSubmit(e) {
+  e.preventDefault();
+
+  let delayValue = Number(form.delay.value);
+
+  for (let i = 0; i < form.amount.value; i++) {
+     createPromise(i, delayValue)
+      .then(({ position, delay }) => {
+        Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`,);
+      });
+    
+  }
+  delayValue+=Number(form.setPointerCapture.value)
 }
